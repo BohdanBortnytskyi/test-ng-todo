fc6a915a2920837dd91e6665636c86942e7ac03f
Bohdan <bobo.kiev@gmail.com>
2020-11-27 01:59:07 +0200

    Safe CRLF Off

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-app';
+
+  public taskItems = [];
+
+  public newTaskTitle;
+  public newTaskText;
+
+  public addTask() {
+    if (this.newTaskTitle != '') {
+      let task = {
+        title: this.newTaskTitle,
+        text: this.newTaskText
+      };
+
+      this.taskItems.push(task);
+      this.newTaskTitle = '';
+      this.newTaskText = '';
+    }
+  }
+
+  public deleteTask(index) {
+    this.taskItems.splice(index, 1);
+  }
 }
